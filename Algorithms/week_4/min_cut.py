83bd2f6d0c69792601ad35c66aea9e9e2691d9a7
LorenArthur <Loren.sarthur@gmail.com>
2017-08-03 18:40:40 -0700

    Added comments to min_cut

--- Algorithms/week_4/min_cut.py
+++ Algorithms/week_4/min_cut.py
@@ -1,25 +1,47 @@
 import math
 import random
 import copy
-
+# The random contraction algorithm chooses random nodes in the graph to combine
+# and then removes all self references to the nodes. The final number of vertices
+# between the two nodes is refered to as the minimum cut. min_cut uses the random 
+# contraction algorithm which has a low probabability of sucess a number of times
+# dependant on the size of the graph which provides a high expected value of 
+# returning the correct solution. 
 def min_cut(graph):
     mins = []
     for x in range(1,100):
+        # get a copy of the graph to use in this iteration
         g = copy.deepcopy(graph)
         # import IPython; IPython.embed()
+        # iterate until only two super nodes remain
         while len(g) > 2:
+            # shuffle the graph in order to select a random node
             random.shuffle(g)
+            # remove the node from the graph
             n = g.pop()
+            # get a list of indices of nodes in the graph that are with vertices 
+            # to the chosen node
             nodes = [i for i in range(len(g)) if g[i][0][0] in n]
+            # select a random vertice
             i = random.randint(0,len(nodes)-1)
+            # get the node on the head of the vertex
             node = g[nodes[i]]
+            # remove that node from the graph
             g.remove(node)
+            # get the node out of the adjaceny list
+            #  and attach it to the list of nodes in the super node
             node.reverse()
             n[0].extend(node.pop())
+            # build a list of nodes that vertices would make a self reference to
+            # in the super node
             exclude = copy.deepcopy(n[0])
             exclude.extend(n[1:])
+            # extend the super nodes vertices with the vertices that would not 
+            # cause self references
             n.extend([x for x in node if x not in exclude])
+            # reattach the super node to the graph
             g.append(n)
+        # remove any self references from the original list of vertices  
         for item in g:
             to_remove = []
             for val in item[1:]:
@@ -31,8 +53,10 @@ def min_cut(graph):
         print '\n'
         result = g[0][1:]
         result.extend(g[1][1:])
+        # count the number of vertices and add it to the list of found min_cuts
         minimum = len(result)
         mins.append(minimum)       
+    # return the lowest min_cut found
     print(min(mins))
     return min(mins)
 

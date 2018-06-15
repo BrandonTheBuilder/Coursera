90e38282190f074f005fa097f7be6a94c5e8a78c
Loren Arthur <loren.sarthur@gmail.com>
2018-06-14 18:24:54 -0700

    Overloaded [] operator

--- Algorithms/week_4/src/graph.cpp
+++ Algorithms/week_4/src/graph.cpp
@@ -1,7 +1,18 @@
 #include "graph.h"
 
 Graph::Graph(const std::string& fileName){
-
+    std::fstream input(fileName);
+    std::string line;
+    while(!input.eof()) {
+        getline(input, line);
+        std::istringstream buffer(line);
+        std::vector<int> data = {};
+        int num;
+        while(buffer >> num){
+            data.push_back(num);
+        }
+        _graph.push_back(data);
+    }
 }
 
 int Graph::getMinCut() {
--- Algorithms/week_4/src/graph.h
+++ Algorithms/week_4/src/graph.h
@@ -3,12 +3,14 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 class Graph {
 public:
     Graph(const std::string& fileName);
     int getMinCut();
+    std::vector<int> operator[](int i){return _graph[i];}
 private:
     void combineNodes();
-    std::vector<std::vector<int>> _graph;
+    std::vector<std::vector<int>> _graph = {};
 };

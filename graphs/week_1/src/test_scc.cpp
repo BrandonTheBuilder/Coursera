b55ce1a5a42180f23d96ce31aed2105444eb6560
Loren Arthur <loren.sarthur@gmail.com>
2018-09-08 12:34:45 -0700

    Added basic test for strongly connected components

---
+++ graphs/week_1/src/test_scc.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "scc.h"
+
+TEST(getFinishingTime, simpleInput) {
+    std::vector<std::vector<int>> graph_input = {
+        {0,1,0},
+        {1,0,0},
+        {0,1,0},
+    };
+}
